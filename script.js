977709a221de9f76f710176719ba09c4ac3e8bad
Sudarshan Natesan <101020179+DG2fut@users.noreply.github.com>
2023-01-03 16:36:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const sounds = ["amogus1","amogus2","amogus3","amogus4"]
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopPlaying();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById("buttons").append(btn);
+})
+
+function stopPlaying() {
+    sounds.forEach(sound => {
+        const audio = document.getElementById(sound);
+        audio.pause();
+        audio.currentTime = 0;
+    })
+}
